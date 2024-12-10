48123d1cfc740283bc14b3b357cb9daecae8ea06
catalamarti <marticatalasabate@gmail.com>
2024-12-10 11:48:47 +0000

    json -> csv

--- Study/Codelists/createCodelists.R
+++ Study/Codelists/createCodelists.R
@@ -1,19 +1,3 @@
-# Connection details
-db <- DBI::dbConnect("...")
-
-# connection details
-databaseAcronym <- "..."
-cdmDatabaseSchema <- "..."
-resultsDatabaseSchema <- "..."
-resultsStem <- "..."
-
-cdm <- CDMConnector::cdmFromCon(
-  con = db,
-  cdmSchema = cdmDatabaseSchema,
-  writeSchema = resultsDatabaseSchema,
-  cdmName = databaseAcronym,
-  writePrefix = resultsStem
-)
 
 ingredients <- c(
   "lovastatin", "pravastatin", "fluvastatin", "atorvastatin", "cerivastatin",
@@ -22,17 +6,29 @@ ingredients <- c(
 )
 
 codes <- CodelistGenerator::getDrugIngredientCodes(
-  cdm = cdm, name = ingredients
+  cdm = cdm, name = ingredients, nameStyle = "{concept_name}"
 )
 
-omopgenerics::exportCodelist(codes, path = here::here("Ingredients"), type = "csv")
+omopgenerics::exportCodelist(
+  x = codes, path = here::here("Codelists", "Ingredients"), type = "csv"
+)
 
 # convert json files to csv
-codes <- CodelistGenerator::codesFromConceptSet(path = here::here("Conditions"), cdm = cdm)
-omopgenerics::exportCodelist(codes, path = here::here("Conditions"), type = "csv")
-
-codes <- omopgenerics::importCodelist(here::here("Indications"))
-omopgenerics::exportCodelist(codes, path = here::here("Indications"), type = "csv")
-
-codes <- omopgenerics::importCodelist(here::here("Medications"))
-omopgenerics::exportCodelist(codes, path = here::here("Medications"), type = "csv")
+# codes <- CodelistGenerator::codesFromConceptSet(
+#   path = here::here("Codelists", "Conditions"), cdm = cdm
+# )
+# omopgenerics::exportCodelist(
+#   codes, path = here::here("Codelists", "Conditions"), type = "csv"
+# )
+#
+# codes <- omopgenerics::importCodelist(here::here("Codelists", "Indication"))
+# omopgenerics::exportCodelist(
+#   codes, path = here::here("Codelists", "Indication"), type = "csv"
+# )
+#
+# codes <- CodelistGenerator::codesFromConceptSet(
+#   path = here::here("Codelists", "Medications"), cdm = cdm
+# )
+# omopgenerics::exportCodelist(
+#   codes, path = here::here("Codelists", "Medications"), type = "csv"
+# )
