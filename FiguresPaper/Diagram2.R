b44a1f573ac2f9714d6f6e7b5d16e2f47ad0f660
catalamarti <marticatalasabate@gmail.com>
2025-07-02 13:11:41 +0100

    diagram 2

--- FiguresPaper/Diagram2.R
+++ FiguresPaper/Diagram2.R
@@ -1,9 +1,9 @@
 
 cole <- "#66A3FF"
 colw <- "black"
-colh <- "#279122"
-pain <- ""
-unknown <- ""
+colh <- "#2A9D8F"
+colp <- "#F4A261"
+colu <- "#8E44AD"
 
 exposures <- dplyr::tibble(
   person_id = 1:5L,
@@ -19,10 +19,22 @@ pain <- dplyr::tibble(
   person_id = c(2, 4, 5),
   x = c(0, -10, 1)
 )
+unknown <- dplyr::tibble(
+  person_id = c(2, 3, 3, 4, 5),
+  x = c(-15, -10, -20, -40, 20)
+)
 
 headache <- headache |>
   dplyr::inner_join(exposures, by = "person_id") |>
   dplyr::mutate(x = x + start, type = "Headache") |>
+  dplyr::select(!c("start", "end"))
+pain <- pain |>
+  dplyr::inner_join(exposures, by = "person_id") |>
+  dplyr::mutate(x = x + start, type = "Pain") |>
+  dplyr::select(!c("start", "end"))
+unknown <- unknown |>
+  dplyr::inner_join(exposures, by = "person_id") |>
+  dplyr::mutate(x = x + start, type = "Unknown") |>
   dplyr::select(!c("start", "end"))
 
 window <- c(-30, 7)
@@ -38,54 +50,85 @@ windows <- exposures |>
 exposures <- exposures |>
   tidyr::pivot_longer(c("start", "end"), names_to = NULL, values_to = "x")
 
-ggplot2::ggplot() +
+p <- ggplot2::ggplot() +
   # exposures
   ggplot2::geom_line(
     mapping = ggplot2::aes(x = x, y = person_id, group = person_id, color = type),
     data = exposures,
     size = 2,
+    inherit.aes = FALSE
   ) +
   # windows
   ggplot2::geom_point(
     mapping = ggplot2::aes(x = x, y = person_id, group = person_id, color = type),
     data = windows,
-    size = 1
+    size = 1,
+    inherit.aes = FALSE
   ) +
   ggplot2::geom_errorbar(
     mapping = ggplot2::aes(xmin = xmin, xmax = xmax, y = person_id, group = person_id, color = type),
     data = windows,
     size = 0.5,
-    width = 0.1
+    width = 0.1,
+    inherit.aes = FALSE
   ) +
   # headache
   ggplot2::geom_point(
-    mapping = ggplot2::aes(x = x, y = person_id, group = person_id, color = type),
+    mapping = ggplot2::aes(x = x, y = person_id, group = person_id, fill = type),
     data = headache,
-    shape = 13,
-    size = 4
+    size = 4,
+    shape = 23,
+    stroke = 0.3,
+    colour = "black",
+    inherit.aes = FALSE
+  ) +
+  # pain
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(x = x, y = person_id, group = person_id, fill = type),
+    data = pain,
+    size = 4,
+    shape = 23,
+    stroke = 0.3,
+    colour = "black",
+    inherit.aes = FALSE
+  ) +
+  # unknown
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(x = x, y = person_id, group = person_id, fill = type),
+    data = unknown,
+    size = 4,
+    shape = 23,
+    stroke = 0.3,
+    colour = "black",
+    inherit.aes = FALSE
   ) +
   # legend
   ggplot2::scale_color_manual(
     name = NULL,
-    values = c("Exposures" = cole, "Indication window" = colw, "Headache" = colh)
+    values = c("Exposures" = cole, "Indication window" = colw)
   ) +
-  ggplot2::scale_y_continuous(breaks = NULL, name = "") +
+  ggplot2::scale_fill_manual(
+    name = NULL,
+    values = c("Headache" = colh, "Pain" = colp, "Unknown" = colu)
+  ) +
+  ggplot2::scale_y_continuous(breaks = NULL, name = "", limits = c(0.5, 5.5)) +
   ggplot2::scale_x_continuous(name = "Time (days)") +
   ggplot2::theme(
     legend.position = "top",
     axis.line.x = ggplot2::element_line(linewidth = 0.5),
-    legend.text = ggplot2::element_text(size = 14),
+    legend.text = ggplot2::element_text(size = 12),
     plot.background = ggplot2::element_rect(fill = "white", color = NA),
-    panel.background = ggplot2::element_rect(fill = "white", color = NA)
+    panel.background = ggplot2::element_rect(fill = "white", color = NA),
+    text = ggplot2::element_text(size = 14, family = "Graphik")
   )
 
 p
 
 ggplot2::ggsave(
-  filename = "./Figures/Diagram1.png",
+  filename = "./Figures/Diagram2.png",
   plot = p,
-  width = 740*3,
-  height = 344*3,
+  width = 662*3,
+  height = 426*3,
   units = "px",
   dpi = 300
 )
